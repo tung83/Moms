41d7a64fac24768dee05001008e55cd92cb96351
Tung Nguyen <tungng@augensoftwaregroup.com>
2017-06-23 15:10:37 +0700

    Add project files.

---
+++ Moms/Moms/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Moms.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Moms/Moms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Moms.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Moms.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
